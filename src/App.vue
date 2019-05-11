0b7016f8e76b64b31d1c0400ef09f1ca05d382e1
David Watson <project7@Davids-iMac.local>
2019-05-11 15:03:39 -0400

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
+/* eslint-disable */
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import QuoteGrid from './components/QuoteGrid.vue'
+import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      quotes: [
+        'Just a random amazing quote!',
+      ],
+      maxQuotes: 10
+    }
+  },
+  methods:{
+    newQuote(quote){
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please Delete Quotes First")
+      }
+      this.quotes.push(quote)
+    },
+    deleteQuote(index){
+      this.quotes.splice(index, 1);
+    }
+  },
   components: {
-    HelloWorld
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+/* eslint-disable */
+<template>
+    <div class="row">
+        <app-quote v-for="(quote, index) in quotes" :key="quote" @click.native="deleteQuote(index)"> {{ quote }}</app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+
+export default {
+    props: ['quotes'],
+    methods: {
+        deleteQuote(index){
+            this.$emit('quoteDeleted', index);
+        }
+    },
+    components: {
+        appQuote: Quote
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
